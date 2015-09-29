0f069f8112cac8ad6e4116cee7abfb2bd83d82e4
Nathan Boxhall-Burnett <u1254544@unimail.hud.ac.uk>
2015-09-29 12:25:48 +0100

    Added initial game files.

---
+++ marsclicker/Assets/clickme.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickme : MonoBehaviour {
+	float money;
+
+	// Use this for initialization
+	void Start () {
+		//initialisation
+		money = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonDown(0)){
+			RaycastHit hit = new RaycastHit();
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Transform select = GameObject.FindWithTag("mars").transform;
+			if (Physics.Raycast (ray, out hit, 100.0f)){
+				money += 1.2f;
+			}
+		}
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(10, 10, 100, 20), "$" + Mathf.Floor(money).ToString());
+	}
+}
---
+++ marsclicker/Assets/rotatorbit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatorbit : MonoBehaviour {
+	public float rotY = 0f;
+	public float rotX = 0f;
+	public float rotZ = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.rotation = Quaternion.AngleAxis(rotY--, new Vector3(-0.4f, 1, 0));
+
+	}
+}
