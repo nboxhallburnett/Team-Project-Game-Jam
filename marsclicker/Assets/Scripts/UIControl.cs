dd5bb584a450a2c8ba8c2232c71d3770fefad6ea
Nathan Boxhall-Burnett <nathan.bb@live.co.uk>
2015-10-11 23:08:25 +0100

    Re-worked the Menu scene to match mock-ups and adjust to orientation changes.

--- marsclicker/Assets/Scripts/UIControl.cs
+++ marsclicker/Assets/Scripts/UIControl.cs
@@ -12,15 +12,19 @@ public class UIControl : MonoBehaviour {
         if (Screen.width >= Screen.height) {
             landscapeCanvas.enabled = true;
             portraitCanvas.enabled = false;
-            
-            planet.transform.position = new Vector3(-3.333f, 0, 0);
-            planet.transform.localScale = new Vector3(5, 5, 5);
+
+            if (planet != null) {
+                planet.transform.position = new Vector3(-3.333f, 0, 0);
+                planet.transform.localScale = new Vector3(5, 5, 5);
+            }
         } else {
             landscapeCanvas.enabled = false;
             portraitCanvas.enabled = true;
-            
-            planet.transform.position = new Vector3(0, 2, 0);
-            planet.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
+
+            if (planet != null) {
+                planet.transform.position = new Vector3(0, 2, 0);
+                planet.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
+            }
         }
     }
 }
