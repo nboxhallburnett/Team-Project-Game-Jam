1638299b6b8a8becba85e4ef111dae6cd3be9be5
Daniel Howlett <littledan7@hotmail.com>
2015-10-16 22:55:00 +0100

    Added a new multiplier pickup so now the PickupManager randomly selects between spawning a cash bonus pickup and a multiplier. Made other minor changes to the scenes and prefabs (setting default values of scripts etc)

--- marsclicker/Assets/Scripts/Pickups/PickupManager.cs
+++ marsclicker/Assets/Scripts/Pickups/PickupManager.cs
@@ -2,15 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
 public class PickupManager : MonoBehaviour {
 
     public Texture2D PickupTexture;
     public List<Pickup> ActivePickups = new List<Pickup>();
-    public float SpawnTimeInterval = 100.0f;
+    public bool IsGameScene;
+    public float SpawnTimeInterval = 5.0f;
 
     public float Timer { get; private set; }
-    public float LastSpawnTime { get; private set; }
-
+    public float LastSpawnTime { get; private set; }    
 
 	// Use this for initialization
 	void Start () {
@@ -19,26 +20,29 @@ public class PickupManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Timer += Time.deltaTime;
-        HandlePickupSpawns();
-        foreach(Pickup pickup in ActivePickups.ToArray())
+        if (IsGameScene)
         {
-            pickup.UpdateOpacity();
-            if (pickup.IsActive)
+            Timer += Time.deltaTime;
+            HandlePickupSpawns();
+            foreach (Pickup pickup in ActivePickups.ToArray())
             {
-                if (pickup.IsCollected())
+                pickup.UpdateOpacity();
+                if (pickup.IsActive)
                 {
-                    pickup.OnCollect();
+                    if (pickup.IsCollected())
+                    {
+                        pickup.OnCollect();
+                        ActivePickups.Remove(pickup);
+                        Destroy(pickup.PickupObject);
+                    }
+                }
+                else
+                {
                     ActivePickups.Remove(pickup);
                     Destroy(pickup.PickupObject);
                 }
-            }
-            else
-            {
-                ActivePickups.Remove(pickup);
-                Destroy(pickup.PickupObject);
-            }
 
+            }
         }
 	}
 
@@ -49,8 +53,18 @@ public class PickupManager : MonoBehaviour {
             float xPos = Random.Range(0, Screen.width - PickupTexture.width);
             float yPos = Random.Range(0, Screen.height);
             LastSpawnTime = Timer;
-            MoneyBonusPickup pickup = new MoneyBonusPickup(xPos, yPos, PickupTexture);
-            ActivePickups.Add(pickup);
+
+            float pickupNumber = Random.Range(0f, 2f);
+            if(pickupNumber <= 1f)
+            {
+                MoneyBonusPickup pickup = new MoneyBonusPickup(xPos, yPos, PickupTexture);
+                ActivePickups.Add(pickup);
+            }
+            else if(pickupNumber <= 2f)
+            {
+                MultiplierPickup pickup = new MultiplierPickup(xPos, yPos, PickupTexture);
+                ActivePickups.Add(pickup);
+            }
 
             SpawnTimeInterval = Random.Range(180.0f, 240.0f);
         }
